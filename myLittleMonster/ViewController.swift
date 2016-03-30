8825cc97c77c64aa378ffc787d8921a717b9a4c0
Gabe Hoffman <gabehoffman@me.com>
2016-03-30 16:28:22 -0400

    part 2 completed

--- myLittleMonster/ViewController.swift
+++ myLittleMonster/ViewController.swift
@@ -11,6 +11,8 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var monsterImg: UIImageView!
+    @IBOutlet weak var foodImg: DragImg!
+    @IBOutlet weak var healthImg: DragImg!
     
     override func viewDidLoad() {
         super.viewDidLoad()
